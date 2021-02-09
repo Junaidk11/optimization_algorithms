8be443f65d522911e9660137cbab913db8da491c
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2021-02-08 16:18:24 -0800

    Fixed the algorithm, had swapped x1 and x2 around.
    I.e. x1 = upper_boundary - golden_ratio*(upper_boundary - lower_boundary) -> is the correct estimation of x1. I had x1 = upper_boundary + golden_ratio*(upper_boundary - lower_boundary)

--- golden_section/golden_section_method.m
+++ golden_section/golden_section_method.m
@@ -1,16 +1,14 @@
 function golden_section_method(initial_lower, initial_upper, tolerance_desired,iteration_desired)
-figure;
-hold on;
 %% Number of Function evaluation 
 global fcount;
 fcount = 0; 
 
 %% Search Interval 
-x1 = initial_lower; %initial lower bound
-x4 = initial_upper; %initial upper bound
+x_lower = initial_lower; %initial lower bound
+x_upper = initial_upper; %initial upper bound
 
 %% Tolerances
-e = abs(x4-x1); % Current iteration tolerance 
+e = abs(x_upper-x_lower); % Current iteration tolerance 
 e_desired = tolerance_desired; 
 
 %% Iterations
@@ -21,19 +19,21 @@ iteration_limit = iteration_desired;
 k = 2/(1+sqrt(5));
 
 %% Initial Interval 
-interval = x4-x1;
+interval = x_upper-x_lower;
 
 %% Select two points in the 'initial interval'
-x2 = x4-k*interval; % closer to the upper bound - not really but helps visualize
-x3 = x1+k*interval; % closer to the lower bound - not really but helps visualize
-temp1 = myfunc(x2);
-temp2 = myfunc(x3);
+
+x1 = x_upper - k*interval;
+x2 = x_lower + k*interval;
+
+fval_x1 = myfunc(x1);
+fval_x2 = myfunc(x2);
 
 % plotting x
-plot(x1,myfunc(x1),'go');
-plot(x2,temp1,'rx');     
-plot(x3,temp2,'bx');
-plot(x4,myfunc(x4),'go');
+%plot(x_lower, myfunc(x_lower),'bo');
+%plot(x_upper, myfunc(x_upper),'bo');
+%plot(x1, fval_x1, 'rx');
+%plot(x2, fval_x2, 'bo');
 
 while (e>e_desired && n < iteration_limit)
     
@@ -41,38 +41,36 @@ while (e>e_desired && n < iteration_limit)
     %% Evaluate function at two points in the 'interval'       
     n=n+1;  
     %% Check function evaluations to update interval    
-    if temp1<=temp2              
-        x4 = x3; % Upper bound shifted to the left
-        %x1 = x1; % Lower bound not changed -> Redundant statement,  could skip.       
-        x3 = x2;        
-        interval = x4-x1; % Interval for next iteration 
-        x2 = x4 - k*interval;  
+    if fval_x1<=fval_x2              
+        x_upper = x2; % Upper bound shifted to the left
+        %x1 = x1; % Lower bound not changed -> Redundant statement, could skip.       
+        x2 = x1;        
+        interval = x_upper-x_lower; % Interval for next iteration 
+        x1 = x_upper - k*interval;  
         
         % New points to check in the new interval for next iteration                                           
-        temp1 = myfunc(x2);
-        temp2 = myfunc(x3);
+        fval_x2 = fval_x1;
+        fval_x1 = myfunc(x1);
         
         %plot new point
-        plot(x2,temp1,'rx');
+        %plot(x1,fval_x1,'rx');
     else       
-        x1 = x2; % Lower Bound shifted to the right
+        x_lower = x1; % Lower Bound shifted to the right
         %x4 = x4; % Upper bound not changed -> redundant statement, could skip 
-        x2 = x3; 
-        interval = x4-x1;% update interval for next iteration
-        x3 = x1 + k*interval; 
+        x1 = x2; 
+        interval = x_upper-x_lower;% update interval for next iteration
+        x2 = x_lower + k*interval; 
         
         % New points to check in the new interval for the next iteration                    
-        temp1 = myfunc(x2); 
-        temp2 = myfunc(x3);
+        fval_x1 = fval_x2; 
+        fval_x2 = myfunc(x2);
         
         %plot new point
-        plot(x3,temp2,'bx');
+        %plot(x2,fval_x2,'bo');
     end    
     
     %% Calculate new Tolerance   
-    e = abs(x4-x1);
-    %% Check algorithm stop conditions
-    n=n+1;           
+    e = abs(x_upper-x_lower);      
 end
 
 if (n == iteration_limit)
@@ -80,12 +78,16 @@ if (n == iteration_limit)
     disp('ERROR: iterations reached max');
 else 
     
-    if(temp1<temp2) 
-        
-        fprintf('Minimum value found is %d found at location x = %d \n',temp1, x2)
+    if(fval_x1<fval_x2) 
+        fprintf('Minimum value found is %d found at location x = %d \n',fval_x1, x1)
+        fprintf('Number of iterations is %d and function calls is %d \n',n, fcount)
+    elseif (fval_x1 >fval_x2)
+        fprintf('Minimum value found is %d found at location x = %d \n',fval_x2, x2)
         fprintf('Number of iterations is %d and function calls is %d \n',n, fcount)
     else
-        fprintf('Minimum value found is %d found at location x = %d \n',temp2, x3)
+        % Either one of x1 and x2, as they're both giving the same point -
+        % Won't reach this clause. Placed it for the sake of completeness.
+        fprintf('Minimum value found is %d found at location x = %d \n',fval_x2, x2)
         fprintf('Number of iterations is %d and function calls is %d \n',n, fcount)
     end
     
